b58ba01795285ec253bd7762372368590b1dcbdc
mini2317 <55321979+mini2317@users.noreply.github.com>
2025-12-06 11:55:43 +0000

    discord 관련 버그 수정

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
 git fetch --all
 git reset --hard origin/main
+source .venv/bin/activate
+pip uninstall discord discord.py -y
+pip install -r requirements.txt
 sudo systemctl daemon-reload
 sudo systemctl restart pynapse
-echo "봇 재시작 완료"
+echo "✅ 봇이 성공적으로 재시작되었습니다."
